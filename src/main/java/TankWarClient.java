7c888e113bc106b17dc5be0f18b27f84edbb0fa0
zhuzhu <490919407@qq.com>
2019-11-17 16:11:45 +0800

    Tank_War 0.2,使坦克动起来

--- src/main/java/TankWarClient.java
+++ src/main/java/TankWarClient.java
@@ -4,6 +4,16 @@ import java.awt.event.WindowEvent;
 
 public class TankWarClient extends Frame {
 
+    int x= 50,y=50;
+
+    public void paint(Graphics g){
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x,y,30,30);
+        g.setColor(c);
+
+        y+=5;
+    }
     /**
      * 设置边框
      */
@@ -19,11 +29,29 @@ public class TankWarClient extends Frame {
         });
         //禁止改变边框大小
         this.setResizable(false);
+        this.setBackground(Color.GREEN);
         setVisible(true);
+        new Thread(new PrintThread()).start();
     }
 
     public static void main(String[] args) {
         TankWarClient client = new TankWarClient();
         client.lauchFrame();
+    }
+
+    /**
+     * 写一个内部类线程来驱动坦克
+     */
+    private class PrintThread implements Runnable{
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(50);
+                    repaint();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
